93f18f328e2179c006570f402797b528fc57122d
RobinKnol <robin@knol.email>
2020-04-06 18:20:19 +0200

    Finish

--- Ajax/Script.js
+++ Ajax/Script.js
@@ -1,30 +1,21 @@
-// bind de HTML id's
-let firstName = document.getElementById("firstName").Value;
-let lastName = document.getElementById("lastName").Value;
-let age = document.getElementById("age").Value;
-let email = document.getElementById("email").Value;
-let submitButton = document.getElementById("submitButton");
-let responseHere = document.getElementById("responseHere");
-
-// click event listener
-// submitButton.addEventListener('click', ajax);
-
-// ajax functie
 function ajax()
 {
-    let xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function()
+
+    var ajaxCall = new XMLHttpRequest();
+    ajaxCall.onreadystatechange = function()
     {
-        if (this.readyState == 4 && this.status == 200)
+        if (ajaxCall.readyState === 4)
         {
-            responseHere.innerHTML = this.responseText;
+            document.getElementById("responseHere").innerHTML = ajaxCall.responseText;
         }
     }
+    
+    let firstName = document.getElementById("firstName").value;
+    let lastName = document.getElementById("lastName").value;
+    let age = document.getElementById("age").value;
+    let email = document.getElementById("email").value;
     let queryString = "?firstName=" + firstName + "&lastName=" + lastName + "&age=" + age + "&email=" + email;
     
-    // debuggen laat de http string zien
-    console.log(queryString);
-    // bereid de ajax actie voor
-    xmlhttp.open("GET", "form_1.php" + queryString, true);
-    xmlhttp.send(null); // doe het
+    ajaxCall.open("GET", "form.php" + queryString, true);
+    ajaxCall.send(null);
 }
